b40c6121a4949c2faf0634e1732e157ca814d745
Petrescu Cristian <cristian.petrescu@mta.ro>
2023-11-23 22:36:32 +0200

    Login Page Created

---
+++ src/main/java/ro/client_sign_app/clientapp/LoginPage.java
@@ -0,0 +1,23 @@
+package ro.client_sign_app.clientapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginPage extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login-view.fxml"));
+        primaryStage.setTitle("Login Page");
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
